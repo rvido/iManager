54638902f358509c0bf528e88bcd9dfdd29bc40a
Richard Vidal-Dorsch <richard.dorsch@advantech.com>
2015-12-14 18:28:12 -0800

    Remove copyright notice in compat.h

--- compat.h
+++ compat.h
@@ -1,9 +1,6 @@
 /*
  * Platform device compatibility
  *
- * Copyright (C) 2015 Advantech Co., Ltd., Irvine, CA, USA
- * Author: Richard Vidal-Dorsch <richard.dorsch@advantech.com>
- *
  * This program is free software; you can redistribute  it and/or modify it
  * under  the terms of  the GNU General  Public License as published by the
  * Free Software Foundation;  either version 2 of the  License, or (at your
