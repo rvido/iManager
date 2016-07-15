01f407b6bc283d014a72f07a5105bc8e16616ff6
Richard Vidal-Dorsch <richard.dorsch@gmail.com>
2016-07-14 21:07:19 -0700

    Update renamed offset defines se 'bd' (backlight device) instead of 'bl'

--- imanager_bl.c
+++ imanager_bl.c
@@ -2,7 +2,7 @@
  * Advantech iManager Backlight driver
  * Partially derived from wm831x_bl
  *
- * Copyright (C) 2016 Advantech Co., Ltd., Irvine, CA, USA
+ * Copyright (C) 2016 Advantech Co., Ltd.
  * Author: Richard Vidal-Dorsch <richard.dorsch@advantech.com>
  *
  * This program is free software; you can redistribute  it and/or modify it
@@ -14,22 +14,19 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
 #include <linux/backlight.h>
+#include <linux/device.h>
 #include <linux/fb.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/platform_device.h>
 #include <linux/pwm.h>
-#include <linux/slab.h>
 #include "compat.h"
 #include "imanager.h"
 
 #define BL_MAX_PWM	100
 
-enum backlight_units {
-	BL_UNIT_1 = 0,
-	BL_UNIT_2,
-};
+enum backlight_units { BL_UNIT_1 = 0, BL_UNIT_2 };
 
 static bool polarity = PWM_POLARITY_NORMAL;
 module_param(polarity, bool, 0);
@@ -42,7 +39,7 @@ MODULE_PARM_DESC(unit, "Select backlight control unit [0, 1] (defaults to 0)");
 struct imanager_backlight_data {
 	struct imanager_device_data *imgr;
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,13,0)
-	struct backlight_device *bl;
+	struct backlight_device *bd;
 #endif
 };
 
@@ -82,20 +79,20 @@ static int imanager_bl_set_polarity(struct imanager_io_ops *io, uint polarity)
 	struct backlight_ctrl *ctrl = (struct backlight_ctrl *)&val8;
 	int ret;
 
-	ret = imanager_read_ram(io, EC_RAM_ACPI, EC_ACPIRAM_BLC_CTRL,
+	ret = imanager_read_ram(io, EC_RAM_ACPI, EC_OFFSET_BACKLIGHT_CTRL,
 				&val8, sizeof(val8));
 	if (ret < 0)
 		return ret;
 
 	ctrl->blpol = polarity ? 1 : 0;
 
-	return imanager_write_ram(io, EC_RAM_ACPI, EC_ACPIRAM_BLC_CTRL,
+	return imanager_write_ram(io, EC_RAM_ACPI, EC_OFFSET_BACKLIGHT_CTRL,
 				  &val8, sizeof(val8));
 }
 
-static int imanager_bl_get_brightness(struct backlight_device *bl)
+static int imanager_bl_get_brightness(struct backlight_device *bd)
 {
-	struct imanager_backlight_data *data = bl_get_data(bl);
+	struct imanager_backlight_data *data = bl_get_data(bd);
 	struct imanager_device_data *imgr = data->imgr;
 	struct imanager_io_ops *io = &imgr->ec.io;
 	u8 devid = imgr->ec.bl.attr[unit].did;
@@ -105,7 +102,7 @@ static int imanager_bl_get_brightness(struct backlight_device *bl)
 
 	pwm = imanager_read8(io, EC_CMD_HWP_RD, devid);
 	if (pwm < 0) {
-		dev_warn(&bl->dev, "Failed while reading PWM\n");
+		dev_warn(&bd->dev, "Failed while reading PWM\n");
 		pwm = 0;
 	}
 
@@ -114,33 +111,29 @@ static int imanager_bl_get_brightness(struct backlight_device *bl)
 	return polarity ? BL_MAX_PWM - pwm : pwm;
 }
 
-static int imanager_bl_set_brightness(struct backlight_device *bl)
+static int imanager_bl_set_brightness(struct backlight_device *bd)
 {
-	struct imanager_backlight_data *data = bl_get_data(bl);
+	struct imanager_backlight_data *data = bl_get_data(bd);
 	struct imanager_device_data *imgr = data->imgr;
 	struct imanager_io_ops *io = &imgr->ec.io;
 	u8 devid = imgr->ec.bl.attr[unit].did;
-	u8 brightness = bl->props.brightness;
+	u8 brightness = bd->props.brightness;
 	int ret;
 
-	if (bl->props.power != FB_BLANK_UNBLANK)
+	if (bd->props.power != FB_BLANK_UNBLANK)
 		brightness = 0;
 
-	if (bl->props.fb_blank != FB_BLANK_UNBLANK)
+	if (bd->props.fb_blank != FB_BLANK_UNBLANK)
 		brightness = 0;
 
-	if (bl->props.state & BL_CORE_SUSPENDED)
+	if (bd->props.state & BL_CORE_SUSPENDED)
 		brightness = 0;
 
 	/* invert brightness if polarity is set */
 	brightness = polarity ? BL_MAX_PWM - brightness : brightness;
 
 	mutex_lock(&imgr->lock);
-
 	ret = imanager_write8(io, EC_CMD_HWP_WR, devid, brightness);
-	if (ret < 0)
-		dev_warn(&bl->dev, "Failed while writing PWM\n");
-
 	mutex_unlock(&imgr->lock);
 
 	return ret;
@@ -157,9 +150,9 @@ static const struct backlight_ops imanager_bl_ops = {
 };
 
 static int imanager_bl_init(struct device *dev,
-				   struct imanager_backlight_data *data)
+			    struct imanager_backlight_data *data)
 {
-	struct backlight_device *bl;
+	struct backlight_device *bd;
 	struct backlight_properties props;
 
 	memset(&props, 0, sizeof(props));
@@ -168,10 +161,10 @@ static int imanager_bl_init(struct device *dev,
 #endif
 	props.max_brightness = BL_MAX_PWM;
 #if LINUX_VERSION_CODE > KERNEL_VERSION(3,12,0)
-	bl = devm_backlight_device_register(dev, "imanager-backlight", dev,
+	bd = devm_backlight_device_register(dev, "imanager-backlight", dev,
 					    data, &imanager_bl_ops, &props);
 #else
-	bl = backlight_device_register("imanager-backlight", dev, data,
+	bd = backlight_device_register("imanager_backlight", dev, data,
 #if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,34)
 				       &imanager_bl_ops);
 #else
@@ -179,23 +172,22 @@ static int imanager_bl_init(struct device *dev,
 #endif
 #endif
 
-	if (IS_ERR(bl)) {
+	if (IS_ERR(bd)) {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,13,0)
-		data->bl = NULL;
+		data->bd = NULL;
 #endif
 		dev_err(dev, "Unable to register backlight device\n");
-		return PTR_ERR(bl);
+		return PTR_ERR(bd);
 	}
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(3,13,0)
-	data->bl = bl;
+	data->bd = bd;
 #endif
+	bd->props.brightness = imanager_bl_get_brightness(bd);
+	bd->props.max_brightness = BL_MAX_PWM;
+	bd->props.power = FB_BLANK_UNBLANK;
 
-	bl->props.brightness = imanager_bl_get_brightness(bl);
-	bl->props.max_brightness = BL_MAX_PWM;
-	bl->props.power = FB_BLANK_UNBLANK;
-
-	backlight_update_status(bl);
+	backlight_update_status(bd);
 
 	return 0;
 }
@@ -205,7 +197,6 @@ static int imanager_bl_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct imanager_device_data *imgr = dev_get_drvdata(dev->parent);
 	struct imanager_backlight_data *data;
-	u8 devid = imgr->ec.bl.attr[unit].did;
 	int ret;
 
 	data = devm_kzalloc(dev, sizeof(*data), GFP_KERNEL);
@@ -236,7 +227,7 @@ static int imanager_bl_remove(struct platform_device *pdev)
 {
 	struct imanager_backlight_data *data = dev_get_drvdata(&pdev->dev);
 
-	backlight_device_unregister(data->bl);
+	backlight_device_unregister(data->bd);
 
 	return 0;
  }
